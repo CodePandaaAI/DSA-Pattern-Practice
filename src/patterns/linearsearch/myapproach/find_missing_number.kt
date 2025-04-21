a0fe0eb37f7f39f637d69079957c18e7d2e4c6e2
CodePandaaAI <romitsharmakv@gmail.com>
2025-04-21 15:02:57 +0530

    DSA Linear Search Problems
    1. find first duplicate
    2. find minimum element
    3. find missing number

---
+++ src/patterns/linearsearch/myapproach/find_missing_number.kt
@@ -0,0 +1,20 @@
+package patterns.linearsearch.myapproach
+
+fun main() {
+    val nums = listOf(1, 2, 3, 4, 5, 6)
+    val result = findMissingNo(nums)
+    if(result!= 0)
+    println(result)
+    else println("The sequence is perfect")
+}
+
+fun findMissingNo(nums: List<Int>): Int {
+    var i = 0
+    while (i < nums.size && i!= nums.lastIndex) {
+        if (nums[i] + 1 != nums[i + 1]) {
+            return nums[i] + 1
+        }
+        i++
+    }
+    return 0
+}
