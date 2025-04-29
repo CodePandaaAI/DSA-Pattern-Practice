851153a9b3a84700f6566c3b47d8adc4e96a75f5
CodePandaaAI <romitsharmakv@gmail.com>
2025-04-29 15:10:28 +0530

    DSA BinarySearch Problems
    Updated the find_index_of_first_occ_target.kt
    
    -> It had wrong logic, instead of finding the targets first occurance index, it returns the index of target found anywhere in the code
    -> Now it correctly returns first occurance index

--- src/patterns/binarysearch/myapproach/find_index_of_first_occ_target.kt
+++ src/patterns/binarysearch/myapproach/find_index_of_first_occ_target.kt
@@ -1,7 +1,8 @@
 package patterns.binarysearch.myapproach
 
 fun main() {
-    val nums = listOf(1, 3, 3, 3, 5, 7, 7, 9, 11, 11, 11, 15)
+    val nums =
+        listOf(1, 3, 3, 3, 5, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 11, 15, 18, 20, 25, 26, 90, 120)
     val target = 11
     if (target <= nums[nums.lastIndex] && target >= nums.first()) {
         var start = 0
@@ -9,10 +10,11 @@ fun main() {
 
         while (start <= end) {
             val mid = (start + end) / 2
-            if (nums[mid] == target) {
+            if (nums[mid] == target && nums[mid - 1] != target) {
                 println("Target lies at index $mid")
                 break
-            } else if (nums[mid] > target) end = mid - 1
+            } else if (nums[mid - 1] == target) end = mid - 1
+            else if (nums[mid] > target) end = mid - 1
             else if (nums[mid] < target) start = mid + 1
             else println("Unexpected error")
         }
