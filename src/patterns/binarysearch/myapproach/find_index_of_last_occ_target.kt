394da7d26b7f1d0e1edb3ca0c21afc3dd9b43b4e
CodePandaaAI <romitsharmakv@gmail.com>
2025-04-29 16:19:48 +0530

    DSA BinarySearch Problems
     Updated find_index_of_last_occ_target.kt
     -> Now this file is using only binary search to find last index of target occurance

--- src/patterns/binarysearch/myapproach/find_index_of_last_occ_target.kt
+++ src/patterns/binarysearch/myapproach/find_index_of_last_occ_target.kt
@@ -1,30 +1,23 @@
 package patterns.binarysearch.myapproach
 
 fun main() {
-    val nums = listOf(1, 3, 3, 3, 5, 7, 7, 9, 11, 11, 11, 15)
-    val target = 11
+    val nums = listOf(1, 2, 3, 3, 3, 5, 7, 7, 9, 11, 11, 11, 11, 15)
+    val target = 3
 
     var start = 0
     var end = nums.lastIndex
-    var targetLastIndex = 0
 
-    val mid = (start + end) / 2
-    when {
-        nums[mid] > target -> {
+    while (start <= end) {
+        val mid = (start + end) / 2
+        if (mid > 0 && nums[mid] == target && nums[mid - 1] != target) {
+            println("target found at index $mid")
+            break
+        } else if (mid > 0 && nums[mid - 1] == target) {
             end = mid - 1
-        }
-
-        nums[mid] < target -> {
+        } else if (nums[mid] > target) {
+            end = mid - 1
+        } else if (nums[mid] < target) {
             start = mid + 1
-        }
-
-        else -> {
-            println("unexpected")
-        }
+        } else println("Unexpected")
     }
-    while(start<=end){
-        if (nums[start] == target) targetLastIndex = start
-        start++
-    }
-    println(targetLastIndex)
 }
