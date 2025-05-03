96a5a86d2e91b75001b9d43354c06f1bc6132680
CodePandaaAI <romitsharmakv@gmail.com>
2025-05-03 20:46:19 +0530

    DSA BinarySearch Problems
    Solved #10: find_floor_ceil.kt
    
    Added myApproach and Online version of:
    Find Floor and Ceil of Target

---
+++ src/patterns/binarysearch/myapproach/find_floor_ceil.kt
@@ -0,0 +1,28 @@
+package patterns.binarysearch.myapproach
+
+fun main() {
+    val nums = listOf(2, 3, 5, 7, 11)
+    var start = 0
+    var end = nums.lastIndex
+    var floor: Int? = null
+    var ceil: Int? = null
+    val target = 9
+
+    while (start <= end) {
+        val mid = (start + end) / 2
+        if(nums[mid] == target){
+            floor = nums[mid]
+            ceil = nums[mid]
+            break
+        }
+        if (nums[mid] > target) {
+            ceil = nums[mid]
+            end = mid - 1
+        }
+        if (nums[mid] < target) {
+            floor = nums[mid]
+            start = mid + 1
+        }
+    }
+    println("floor $floor ceil $ceil")
+}
---
+++ src/patterns/binarysearch/onlinesolution/find_floor_ceil_online.kt
@@ -0,0 +1,32 @@
+package patterns.binarysearch.onlinesolution
+
+fun main() {
+    val nums = listOf(2, 3, 5, 7, 11)
+    val target = 12
+    var start = 0
+    var end = nums.lastIndex
+    var floor: Int? = null
+    var ceil: Int? = null
+
+    while (start <= end) {
+        val mid = (start + end) / 2
+        when {
+            nums[mid] == target -> {
+                floor = nums[mid]
+                ceil = nums[mid]
+                break
+            }
+            nums[mid] < target -> {
+                floor = nums[mid] // mid might be floor
+                start = mid + 1
+            }
+            else -> {
+                ceil = nums[mid] // mid might be ceil
+                end = mid - 1
+            }
+        }
+    }
+
+    println("Floor = $floor")
+    println("Ceil = $ceil")
+}
