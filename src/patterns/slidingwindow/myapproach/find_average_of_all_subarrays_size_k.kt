425a9ddea416f497f46d566f9d1dca8fa10a16e7
CodePandaaAI <romitsharmakv@gmail.com>
2025-05-10 20:37:45 +0530

    Sliding Window Started
    #2 Problem Solved: Find Average of All Subarrays of Size K

---
+++ src/patterns/slidingwindow/myapproach/find_average_of_all_subarrays_size_k.kt
@@ -0,0 +1,19 @@
+package patterns.slidingwindow.myapproach
+
+fun main() {
+    val arr = listOf(1, 3, 2, 6, -1, 4, 1, 8, 2)
+    val k = 5
+    val avgList = mutableListOf<Double>()
+    var current = 0
+
+    for(i in 0..<k){
+        current += arr[i]
+    }
+    avgList.add(current/k.toDouble())
+
+    for(i in k..arr.lastIndex){
+        current = current - arr[i - k] + arr[i]
+        avgList.add(current/k.toDouble())
+    }
+    println(avgList)
+}
--- src/patterns/slidingwindow/myapproach/max_sum_subarray_size_k.kt
+++ src/patterns/slidingwindow/myapproach/max_sum_subarray_size_k.kt
@@ -3,7 +3,7 @@ package patterns.slidingwindow.myapproach
 fun main() {
     // Max sum of subArray of size k
     val arr = listOf(2, 5, 8, 12, 14, 19, 25, 31, 40, 55, 100)
-    val k = 3 // size of window
+    val k = 3 // size of the window
     val result = maxSumSubArray(arr, k)
     println(result)
 }
@@ -11,7 +11,7 @@ fun main() {
 fun maxSumSubArray(arr: List<Int>, k: Int): Int {
     var current = 0
 
-    // Step 1: Calculate sum of first window
+    // Step 1: Calculate a sum of first window
     for (i in 0..<k) {
         current += arr[i]
     }
