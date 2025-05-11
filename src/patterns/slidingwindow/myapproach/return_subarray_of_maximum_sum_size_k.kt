8eea3f958aa3899d113f96f58902121f8338fbdd
CodePandaaAI <romitsharmakv@gmail.com>
2025-05-11 22:48:26 +0530

    Sliding Window Started
    #3 Problem Solved: Maximum Sum Subarray of Size K — BUT RETURN THE ACTUAL SUBARRAY

---
+++ src/patterns/slidingwindow/myapproach/return_subarray_of_maximum_sum_size_k.kt
@@ -0,0 +1,27 @@
+package patterns.slidingwindow.myapproach
+
+fun main() {
+    val arr = listOf(2, 1, 5, 1, 3, 2)
+    val k = 3
+    var current = 0
+    val maxSumList = mutableListOf<Int>()
+
+    for(i in 0..<k) {
+        current += arr[i]
+        maxSumList.add(i)
+    }
+    var max = current
+
+
+    for(i in k..arr.lastIndex){
+        current = current - arr[i-k] + arr[i]
+        if(current> max){
+            maxSumList.clear()
+            maxSumList.add(arr[i-2])
+            maxSumList.add(arr[i-1])
+            maxSumList.add(arr[i])
+            max = current
+        }
+    }
+    println(maxSumList)
+}
