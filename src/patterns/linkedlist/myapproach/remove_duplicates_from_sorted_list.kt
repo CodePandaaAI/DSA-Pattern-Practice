f477e50f1a0266db166cb9cdb42d7cfe90d17973
CodePandaaAI <romitsharmakv@gmail.com>
2025-06-07 19:30:18 +0530

    Linked List
    Solved✅:
    Question #5: Remove Duplicates from sorted List

---
+++ src/patterns/linkedlist/myapproach/remove_duplicates_from_sorted_list.kt
@@ -0,0 +1,37 @@
+package patterns.linkedlist.myapproach
+
+fun main() {
+    val f = Node(1)
+    val s = Node(2)
+    val th = Node(3)
+    val fo = Node(3)
+    val fi = Node(4)
+    f.next = s
+    s.next = th
+    th.next = fo
+    fo.next = fi
+
+    deleteDuplicates(f)
+    var current: Node? = f
+    while(current != null){
+        print("${current.data}-> ")
+        current = current.next
+    }
+    print("null")
+}
+
+fun deleteDuplicates(head: Node?): Node? {
+    val dummy = Node(-1)
+    var current: Node? = head
+    dummy.next = current
+
+    while(current?.next != null) {
+        val next = current.next
+        if(current.next?.data == current.data) current.next = next?.next
+        else {
+            current = current.next
+        }
+    }
+
+    return dummy.next
+}
